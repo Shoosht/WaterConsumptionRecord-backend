c2db14e215a2d5de15402584b6c91245a04dc25b
ToniShoosh <toni.sustar0@gmail.com>
2025-05-19 16:20:18 +0200

    implement user login logic with JWT and validation

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -15,7 +16,41 @@ const userSchema = new Schema({
     }
 })
 
+userSchema.statics.login = async function(email, password) {
+
+    if (!email || !password) {
+        throw Error('All fields must be filled.')
+    }
+    
+    const user = await this.findOne({ email })
+
+    if (!user) {
+        throw Error('Incorrect email.')
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if (!match) {
+        throw Error('Incorrect password')
+    }
+
+    return user
+}
+
 userSchema.statics.signup = async function(email, password) {
+
+    if (!email || !password) {
+        throw Error('All fields must be filled.')
+    }
+
+    if(!validator.isEmail(email)) {
+        throw Error('Email provided is not valid')
+
+    }
+
+    if(!validator.isStrongPassword(password)) {
+        throw Error('Password not strong enough.')
+    }
 
     const exists = await this.findOne({ email })
 
