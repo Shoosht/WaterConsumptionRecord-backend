e32a4f6dc09af2cf7dff3464c44d1e6349368118
ToniShoosh <toni.sustar0@gmail.com>
2025-05-19 03:20:12 +0200

    Added user model, router and controller with password hashing

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const recordRoutes = require('./routes/records')
+const userRoutes = require('./routes/users')
 
 const app = express()
 
 app.use(express.json())
 
 app.use('/api/records', recordRoutes)
+app.use('/api/user', userRoutes)
 
 app.use((req, res, next)=>{
     console.log(req.path, req.method)
