4dac2633fa601e31360b03a0b720ab9b48bb26c8
Shoosht <toni.coding0@gmail.com>
2026-05-11 01:20:25 +0200

    add cors

--- server.js
+++ server.js
@@ -2,23 +2,30 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const recordRoutes = require('./routes/records')
 const userRoutes = require('./routes/users')
 const billRoutes = require('./routes/bills')
 const startReminderCron = require('./utils/sendReminder')
 
+
 const app = express()
 
-app.use(express.json())
+app.use(cors({
+    origin: process.env.FRONTEND_URL
+}))
 
-app.use('/api/records', recordRoutes)
-app.use('/api/user', userRoutes)
-app.use('/api/bills', billRoutes)
+app.use(express.json())
 
 app.use((req, res, next)=>{
     console.log(req.path, req.method)
     next()
 })
+
+app.use('/api/records', recordRoutes)
+app.use('/api/user', userRoutes)
+app.use('/api/bills', billRoutes)
+
 
 mongoose.connect(process.env.MONGODB_URI)
     .then(()=>{
